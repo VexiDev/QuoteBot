c0aa13b55f6b9b9ce0eaf939b9852277f274b913
Vexi <supersushimanlolter@gmail.com>
2021-09-10 13:30:10 -0700

    readded credentials

---
+++ Main.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext.commands import CommandNotFound
+from discord.ext import commands
+import discord
+import os
+
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True)
+bot = commands.Bot(command_prefix='qq!',intents=intents)
+bot.remove_command('help')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send("Unkown command! Use **q!help** for a list of available commands")
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f"loaded {extension} c(p)og")
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send(f"unloaded {extension} c(p)og")
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f"reloaded {extension} c(p)og")
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py') and filename:
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f'Loaded {filename[:-3]}')
+
+@bot.event
+async def on_ready():
+    print("Sucessfully connected to Quotebot!")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="q!help"))
+
+bot.run('ODE0Mzc5MjM5OTMwMzMxMTU3.YDc_xQ.Tud62bhKLaaWGGlpfWr-lJWZe5w')
--- cogs/misc.py
+++ cogs/misc.py
@@ -19,10 +19,10 @@ class Misc(commands.Cog):
 
     def connectdb(self):
         conn = psycopg2.connect(        
-        host="address",
-        database="database name",
-        user="user",
-        password="pass") 
+        host="ec2-107-20-153-39.compute-1.amazonaws.com",
+        database="d54rrbkoagiuqg",
+        user="bcqrzmrdonxkml",
+        password="006986da51bca028a4af7404fde38e18c9f8a6208b495187b93d4744632b652d") 
         return(conn)
 
     @commands.command()
